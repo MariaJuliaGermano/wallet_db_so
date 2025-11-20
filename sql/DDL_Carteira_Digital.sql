8c1cf60aeaf55058c98f4a2fe632ef28c8e4f400
malualbuquerqueb <mlabc@cesar.school>
2025-11-19 21:12:52 -0300

    Add database schema and user creation for wallet_homolog

--- sql/DDL_Carteira_Digital.sql
+++ sql/DDL_Carteira_Digital.sql
@@ -55,11 +55,11 @@ USE wallet_homolog;
 -- ================================
 CREATE TABLE CARTEIRA (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    endereco VARCHAR(255) UNIQUE NOT NULL,
+    endereco_carteira VARCHAR(255) UNIQUE NOT NULL,
     hash_chave_privada VARCHAR(255) NOT NULL,
-    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP
+    data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+    status ENUM ('ATIVA' , 'BLOQUEADA') default 'ATIVA' 
 );
-
 -- ================================
 --  TABELA MOEDA
 -- ================================

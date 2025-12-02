78023263e7a01a7d6b373c59636dde0443490b7c
MariaJuliaGermano <mariajuliapmgermano@gmail.com>
2025-12-02 17:57:36 -0300

    fix: alterando nome das carteiras

--- sql/DDL_Carteira_Digital.sql
+++ sql/DDL_Carteira_Digital.sql
@@ -1,37 +1,9 @@
--- =========================================================
---  Script de criação da base, usuário,
---  Projeto: Carteira Digital
---  Banco:   MySQL 8+
--- =========================================================
-
--- 1) Criação da base de homologação
-CREATE DATABASE IF NOT EXISTS wallet_homolog
-    DEFAULT CHARACTER SET utf8mb4
-    DEFAULT COLLATE utf8mb4_0900_ai_ci;
-
--- 2) Criação do usuário restrito para a API
---    (ajuste a senha conforme necessário)
-
-
-    DROP USER IF EXISTS 'wallet_api_homolog'@'%';
-	CREATE USER 'wallet_api_homolog'@'%' IDENTIFIED BY 'api123';
-    GRANT SELECT, INSERT, UPDATE, DELETE ON wallet_homolog.* TO'wallet_api_homolog'@'%';
-    FLUSH PRIVILEGES;
-
--- 3) Grants: apenas DML (sem CREATE/DROP/ALTER)
-GRANT SELECT, INSERT, UPDATE, DELETE
-    ON wallet_homolog.*
-    TO 'wallet_api_homolog'@'%';
-
-FLUSH PRIVILEGES;
-
--- 4) Usar a base
 USE wallet_homolog;
 
 -- ================================
---  TABELA CARTEIRA
+--  TABELA CARTEIRA (Nome ajustado para minúsculo)
 -- ================================
-CREATE TABLE CARTEIRA (
+CREATE TABLE IF NOT EXISTS carteira (
     id INT AUTO_INCREMENT PRIMARY KEY,
     endereco_carteira VARCHAR(255) UNIQUE NOT NULL,
     hash_chave_privada VARCHAR(255) NOT NULL,
@@ -39,27 +11,27 @@ CREATE TABLE CARTEIRA (
     status ENUM ('ATIVA' , 'BLOQUEADA') default 'ATIVA' 
 );
 
-
 -- ===========================
 --   TABELA: MOEDA
 -- ===========================
-CREATE TABLE MOEDA (
+CREATE TABLE IF NOT EXISTS moeda (
     id_moeda SMALLINT PRIMARY KEY AUTO_INCREMENT,
     codigo VARCHAR(10) NOT NULL UNIQUE,
     nome VARCHAR(50) NOT NULL,
     tipo VARCHAR(10) NOT NULL
 );
 
-INSERT INTO moeda (id_moeda, codigo, nome, tipo) VALUES (1, 'BTC', 'Bitcoin', 'crypto');
-INSERT INTO moeda (id_moeda, codigo, nome, tipo) VALUES (2, 'USD', 'US Dollar', 'currency');
-INSERT INTO moeda (id_moeda, codigo, nome, tipo) VALUES (3, 'ETH', 'Etherium', 'crypto');
-INSERT INTO moeda (id_moeda, codigo, nome, tipo) VALUES (4, 'BRL', 'Real brasileiro ', 'currency');
-INSERT INTO moeda (id_moeda, codigo, nome, tipo) VALUES (5, 'SOL', 'Solana ', 'crypto');
+INSERT INTO moeda (id_moeda, codigo, nome, tipo) VALUES 
+(1, 'BTC', 'Bitcoin', 'crypto'),
+(2, 'USD', 'US Dollar', 'currency'),
+(3, 'ETH', 'Etherium', 'crypto'),
+(4, 'BRL', 'Real brasileiro ', 'currency'),
+(5, 'SOL', 'Solana ', 'crypto');
 
 -- ===========================
 --   TABELA: SALDO_CARTEIRA
 -- ===========================
-CREATE TABLE SALDO_CARTEIRA (
+CREATE TABLE IF NOT EXISTS saldo_carteira (
     endereco_carteira VARCHAR(255) NOT NULL,
     id_moeda SMALLINT NOT NULL,
     saldo DECIMAL(18,8) DEFAULT 0,
@@ -68,36 +40,33 @@ CREATE TABLE SALDO_CARTEIRA (
     PRIMARY KEY (endereco_carteira, id_moeda),
 
     FOREIGN KEY (endereco_carteira)
-        REFERENCES CARTEIRA(endereco_carteira)
+        REFERENCES carteira(endereco_carteira)
         ON DELETE CASCADE,
 
     FOREIGN KEY (id_moeda)
-        REFERENCES MOEDA(id_moeda)
+        REFERENCES moeda(id_moeda)
 );
 
 -- ===========================
 --   TABELA: DEPOSITO_SAQUE
 -- ===========================
-CREATE TABLE DEPOSITO_SAQUE (
+CREATE TABLE IF NOT EXISTS deposito_saque (
     id_movimento BIGINT PRIMARY KEY AUTO_INCREMENT,
     endereco_carteira VARCHAR(100) NOT NULL,
     id_moeda SMALLINT NOT NULL,
-    tipo VARCHAR(20) NOT NULL,  -- "DEPOSITO" ou "SAQUE"
+    tipo VARCHAR(20) NOT NULL,
     valor DECIMAL(18,8) NOT NULL,
     taxa_valor DECIMAL(18,8) DEFAULT 0,
     data_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (endereco_carteira)
-        REFERENCES CARTEIRA(endereco_carteira),
-
-    FOREIGN KEY (id_moeda)
-        REFERENCES MOEDA(id_moeda)
+    FOREIGN KEY (endereco_carteira) REFERENCES carteira(endereco_carteira),
+    FOREIGN KEY (id_moeda) REFERENCES moeda(id_moeda)
 );
 
 -- ===========================
 --   TABELA: CONVERSAO
 -- ===========================
-CREATE TABLE CONVERSAO (
+CREATE TABLE IF NOT EXISTS conversao (
     id_conversao BIGINT PRIMARY KEY AUTO_INCREMENT,
     endereco_carteira VARCHAR(100) NOT NULL,
     id_moeda_origem SMALLINT NOT NULL,
@@ -109,20 +78,15 @@ CREATE TABLE CONVERSAO (
     cotacao_utilizada DECIMAL(18,8) NOT NULL,
     data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (endereco_carteira)
-        REFERENCES CARTEIRA(endereco_carteira),
-
-    FOREIGN KEY (id_moeda_origem)
-        REFERENCES MOEDA(id_moeda),
-
-    FOREIGN KEY (id_moeda_destino)
-        REFERENCES MOEDA(id_moeda)
+    FOREIGN KEY (endereco_carteira) REFERENCES carteira(endereco_carteira),
+    FOREIGN KEY (id_moeda_origem) REFERENCES moeda(id_moeda),
+    FOREIGN KEY (id_moeda_destino) REFERENCES moeda(id_moeda)
 );
 
 -- ===========================
 --   TABELA: TRANSFERENCIA
 -- ===========================
-CREATE TABLE TRANSFERENCIA (
+CREATE TABLE IF NOT EXISTS transferencia (
     id_transferencia BIGINT PRIMARY KEY AUTO_INCREMENT,
     endereco_origem VARCHAR(100) NOT NULL,
     endereco_destino VARCHAR(100) NOT NULL,
@@ -131,14 +95,7 @@ CREATE TABLE TRANSFERENCIA (
     taxa_valor DECIMAL(18,8) NOT NULL,
     data_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (endereco_origem)
-        REFERENCES CARTEIRA(endereco_carteira),
-
-    FOREIGN KEY (endereco_destino)
-        REFERENCES CARTEIRA(endereco_carteira),
-
-    FOREIGN KEY (id_moeda)
-        REFERENCES MOEDA(id_moeda)
+    FOREIGN KEY (endereco_origem) REFERENCES carteira(endereco_carteira),
+    FOREIGN KEY (endereco_destino) REFERENCES carteira(endereco_carteira),
+    FOREIGN KEY (id_moeda) REFERENCES moeda(id_moeda)
 );
-
-SELECT 'Base wallet_homolog e tabelas criadas com sucesso!' AS mensagem;
